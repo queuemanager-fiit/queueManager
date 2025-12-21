adb149dd24004b58d59e7cf4730b64f6b64b9172
Helius-hash <zhoryan.plotn01122003@gmail.com>
2025-12-21 12:11:44 +0500

    test

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7a35d1460f9602b2f0848f188efe0d05aafb0b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8eefa1d1566cd34d9d054b895c66173d08a239c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
